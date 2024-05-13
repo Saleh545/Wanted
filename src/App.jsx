fb324399faeefcca6d0147a2eb55604def216223
Saleh545 <syusifzade545@gmail.com>
2024-05-13 15:27:17 +0400

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./components/header/header.css"
+const App = () => {
+  return (
+    <div>
+      <img src="public/images/hero bckrnd.jpg" alt="" />
+    </div>
+  )
+}
+
+export default App
