c8fce7a0cb6fcfb6d55ec204c4656c0f0ffadfed
Saleh545 <syusifzade545@gmail.com>
2024-05-15 02:04:45 +0400

    finish

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+import { FaInstagram } from "react-icons/fa";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const footer = () => {
+const Footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <div>
+      <div className="footer">
+        <div className="container">
+          <div className="footer-flex">
+            <div className="footer-card">
+              <Link className="logo">wanted</Link>
+              <p>
+                Begin new experiences here – guiding users on transformative
+                journeys of learning and growth.
+              </p>
+              <div className="social">
+              <Link to="https://www.instagram.com" target="blank">
+                  <FaInstagram />
+                </Link>
+              <Link to="https://www.twitter.com" target="blank">
+                  <RiTwitterXFill />
+                </Link>
+              <Link to="https://www.facebook.com" target="blank">
+                  <FaFacebook />
+                </Link>
+              <Link to="https://www.youtube.com" target="blank">
+                  <FaYoutube />
+                </Link>
+              </div>
+            </div>
+            <div className="footer-link">
 
-export default footer
+            <div className="footer-card">
+              <h5>Company</h5>
+              <Link>Find a team member</Link>
+              <Link>Candidates</Link>
+              <Link>Vacancy</Link>
+              <Link>FAQ</Link>
+            </div>
+            <div className="footer-card">
+              <h5>Contact</h5>
+              <Link>Baku, Azerbaijan</Link>
+              <Link to={"mailto:wanted@gmail.com"}>wanted@gmail.com</Link>
+              <Link to={"tel:+994705621269"}>+994 70 562 12 69</Link>
+            </div>
+            </div>
+          </div>{" "}
+          <div className="footer-bottom">
+            <Link>Terms of Use</Link>
+            <Link>Privacy Policy</Link>
+            <p>© 2024 Wanted All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Header from "../../header/Header";
 import { CiSearch } from "react-icons/ci";
 import { Link } from "react-router-dom";
 import { GoArrowRight } from "react-icons/go";
+import { MdOutlineMail } from "react-icons/md";
+import Footer from "../../footer/Footer";
 
 
 const Home = () => {
@@ -36,7 +38,6 @@ const Home = () => {
           <h3>Our collaborated companies</h3>
 
           <div className="companies-cards">
-
             <div className="companies-card">
               <div className="img">
                 <img src="public/images/img-1.png" alt="" />
@@ -44,7 +45,12 @@ const Home = () => {
               <div className="card-text">
                 <h5>Mailchimp</h5>
                 <p>Software Engineering Intern</p>
-                <Link>Get details <i><GoArrowRight /></i></Link>
+                <Link>
+                  Get details{" "}
+                  <i>
+                    <GoArrowRight />
+                  </i>
+                </Link>
               </div>
             </div>
             <div className="companies-card">
@@ -54,7 +60,12 @@ const Home = () => {
               <div className="card-text">
                 <h5>Azercell</h5>
                 <p>Product Management Intern</p>
-                <Link>Get details <i><GoArrowRight /></i></Link>
+                <Link>
+                  Get details{" "}
+                  <i>
+                    <GoArrowRight />
+                  </i>
+                </Link>
               </div>
             </div>
             <div className="companies-card">
@@ -64,7 +75,12 @@ const Home = () => {
               <div className="card-text">
                 <h5>Trello</h5>
                 <p>Human Resources Intern</p>
-                <Link>Get details <i><GoArrowRight /></i></Link>
+                <Link>
+                  Get details{" "}
+                  <i>
+                    <GoArrowRight />
+                  </i>
+                </Link>
               </div>
             </div>
             <div className="companies-card">
@@ -74,18 +90,167 @@ const Home = () => {
               <div className="card-text">
                 <h5>Asana</h5>
                 <p>Graphic Design Intern</p>
-                <Link>Get details <i><GoArrowRight /></i></Link>
+                <Link>
+                  Get details{" "}
+                  <i>
+                    <GoArrowRight />
+                  </i>
+                </Link>
               </div>
             </div>
-
-
           </div>
         </div>
 
         <div className="timeline">
           <h1>Your project timeline:</h1>
+
+          <div className="timeline-img">
+            <img src="public/images/timeline.png" alt="" />
+          </div>
+
+          <div className="timeline-texts">
+            <div className="timeline-text">
+              <h2>Product</h2>
+              <p>
+                The product details page offers all necessary project
+                information, including a clear 4-week timeline. Your main
+                objective is to deliver a viable solution within this timeframe
+              </p>
+            </div>
+            <div className="timeline-text2 timeline-text">
+              <h2>Team</h2>
+              <p>
+                Explore your team's profiles to learn about them, including
+                links to their professional accounts and past projects. Feel
+                free to drop them a message to introduce yourself and say hello!{" "}
+              </p>
+            </div>
+          </div>
+          <div className="timeline-texts">
+            <div className="timeline-text3 timeline-text">
+              <h2>Task Done</h2>
+              <p>
+                Most companies employ task management tools. We've designed one
+                for you to organize and track tasks effectively, showcasing your
+                proficiency in task management to hiring managers!
+              </p>
+            </div>
+            <div className="timeline-text4 timeline-text">
+              <h2>Calendar</h2>
+              <p>
+                Efficient communication within your team is vital. Access our
+                team calendar to view everyone's availability and simplify
+                meeting scheduling. We've already scheduled the first meeting
+                for you!
+              </p>
+            </div>
+          </div>
+          <div className="timeline-texts timeline-texts5">
+            <div className="timeline-text5 timeline-text">
+              <h2>Submit your project</h2>
+              <p>
+                You've completed your project and deployed it. We're excited to
+                see your work! You now have the chance to present it to
+                professionals, gaining valuable feedback to boost your
+                confidence in interviews!
+              </p>
+            </div>
+          </div>
         </div>
       </div>
+      <div className="cv">
+        <div className="container">
+          <div className="cv-flex">
+            <div className="cv-text">
+              <h3>Unlock Your Future: Free CV Upon Internship Completion</h3>
+              <p>
+                We believe in empowering our interns to succeed beyond their
+                internship experience. That's why we offer a special bonus for
+                those who complete their internship online with us a
+                professionally crafted CV, on us! Showcase your newfound skills
+                and experiences with confidence as you transition into the next
+                phase of your career.
+              </p>
+              <Link>Learn more</Link>
+            </div>
+            <div className="cv-img">
+              <img src="public/images/cv.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-center">
+        <div className="container">
+          <div className="card-flex">
+            <div className="card">
+              <img src="public/images/cirle1.png" className="img1" alt="" />
+              <div className="card-img">
+                <img src="public/images/join.png" alt="" />
+              </div>
+              <div className="card-text">
+                <h5>Join team</h5>
+                <span>Hiring managers look for soft skills</span>
+              </div>
+              <img className="img2" src="public/images/circle2.png" alt="" />
+            </div>
+
+            <div className="card">
+              <img src="public/images/circle3.png" className="img3" alt="" />
+              <div className="card-img">
+                <img src="public/images/get.png" alt="" />
+              </div>
+              <div className="card-text">
+                <h5>Get experience</h5>
+                <span>Work-simulated environment</span>
+              </div>
+              <img src="public/images/circle4.png" className="img4" alt="" />
+            </div>
+
+            <div className="card">
+              <img src="public/images/circle5.png" className="img5" alt="" />
+              <div className="card-img">
+                <img src="public/images/find.png" alt="" />
+              </div>
+              <div className="card-text">
+                <h5>Find a job</h5>
+                <span>Apply what you’ve learned</span>
+              </div>
+              <img src="public/images/circle6.png" className="img6" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="intern">
+        <div className="container">
+          <div className="intern-flex">
+            <div className="intern-img">
+              <img src="public/images/qaqas.png" alt="" />
+            </div>
+            <div className="intern-text">
+              <h2>Intern Success: Our Special Bonus Offer</h2>
+              <p>
+                1. <span>Setting Expectations:</span> Clearly outline tasks,
+                responsibilities, and performance standards at the start of the
+                internship.
+              </p>
+              <p>
+                2. <span>Monitoring Progress:</span>  Continuously track interns' performance, offering feedback and support as needed.
+
+              </p>
+              <p>
+                3. <span>Rewarding Achievement:</span> Present a special bonus upon successful completion of the internship, acknowledging their hard work and contributions.
+              </p>
+              <div className="input email">
+            <input type="search" placeholder="Email address" />
+            <i>
+            <MdOutlineMail />            </i>
+          </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer/>
     </div>
   );
 };
